8b78965d9d0828240b89821ef8695b977c3c6364
le16bits <le16bits@gmail.com>
2026-02-17 22:08:13 -0300

    chore: add comentario sobre mock

--- src/main/kotlin/com/ale/devkit/lab/integrations/openWeather/client/WeatherClient.kt
+++ src/main/kotlin/com/ale/devkit/lab/integrations/openWeather/client/WeatherClient.kt
@@ -4,5 +4,6 @@ import com.ale.devkit.lab.integrations.openWeather.controller.request.ParamsRequ
 import com.ale.devkit.lab.integrations.openWeather.controller.response.StormGlassResponse
 
 interface WeatherClient {
+    // Implementação de mock devido a restrição de requests da api -> limite 5/dia: StormGlassMockClient
     fun getWeather(params: ParamsRequest): StormGlassResponse?
 }
